6c685442a2aeb8b09003757e88e4daa8cc06ae96
mariverg <mariverg@student.42malaga.com>
2024-11-06 12:29:03 +0100

    parte obligatoria

---
+++ philo/main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariverg <mariverg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 12:16:21 by mariverg          #+#    #+#             */
+/*   Updated: 2024/11/06 10:14:27 by mariverg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	t_rules	*rules;
+
+	rules = malloc(sizeof(t_rules));
+	if (ac != 5 && ac != 6)
+		error_msg(USAGE);
+	if ((!ft_init_rules(rules, av)))
+		error_msg("threats could not be created");
+	if ((!launcher(rules)))
+		error_msg("something went wrong");
+	exit(0);
+}
+
+bool	launcher(t_rules *rules)
+{
+	int		i;
+	t_philo	*phi;
+
+	i = 0;
+	phi = rules->philo;
+	printf("    Time     Philosopher       Action\n\n");
+	rules->first_timestamp = timestamp();
+	while (i < rules->nb_philo)
+	{
+		if (pthread_create(&(phi[i].thread_id), NULL, philo_thread, &(phi[i])))
+			error_msg("something went wrong creating the threads");
+		phi[i].time_last_eat = timestamp();
+		i++;
+	}
+	death_seeker(rules, rules->philo);
+	finisher(rules);
+	return (true);
+}
+
+void	error_msg(char *str)
+{
+	printf(RED"Error"RESET": %s", str);
+	exit (EXIT_FAILURE);
+}
---
+++ philo/philo.h
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariverg <mariverg@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 12:17:35 by mariverg          #+#    #+#             */
+/*   Updated: 2024/11/06 10:12:10 by mariverg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include "philo.h"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+# define USAGE "Usage: number_of_philosophers, time_to_die, \
+time_to_eat, time_to_sleep, [number_of_times_each_philosopher_must_eat]"
+
+# define RED "\x1B[31m"
+# define RESET "\x1B[0m"
+
+struct	s_rules;
+
+typedef struct s_philo
+{
+	int				philo_id;
+	int				left_fork_id;
+	int				right_fork_id;
+	int				times_ate;
+	long long		time_last_eat;
+	struct s_rules	*rules;
+	pthread_t		thread_id;
+}	t_philo;
+
+typedef struct s_rules
+{
+	int					nb_philo;
+	int					time_death;
+	int					time_to_eat;
+	int					time_sleep;
+	int					nb_eat;
+	long long			first_timestamp;
+	int					philo_feed;
+	bool				all_ate;
+	bool				died;
+	pthread_mutex_t		meal_count_mutex;
+	pthread_mutex_t		eating;
+	pthread_mutex_t		write;
+	pthread_mutex_t		fork[250];
+	t_philo				philo[250];
+}	t_rules;
+
+//main functions
+int				main(int ac, char **av);
+bool			ft_init_rules(t_rules *rules, char **av);
+bool			launcher(t_rules *rules);
+
+//threads
+void	*philo_thread(void *philosopher);
+void	death_seeker(t_rules *rules, t_philo *p);
+void	finisher(t_rules *rules);
+
+//utils & errors
+int				ft_atoi(char *n);
+void			print_moment(t_rules *rules, int id, char *action);
+long long		timestamp(void);
+void			wait_time(t_rules *rules, int time);
+long long		time_taken(long long past, long long present);
+unsigned int	fruit_generator(unsigned long time, int id);
+unsigned long	my_rand(unsigned long fruit);
+void			error_msg(char *str);
+
+#endif
